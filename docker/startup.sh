c6bdfde3aff804368ef1a3f2f7f2e562c44da120
rhyleematte <rhyleematte@gmail.com>
2026-04-10 17:17:14 +0800

    remove static index.html and point server to public root

--- docker/startup.sh
+++ docker/startup.sh
@@ -8,6 +8,6 @@ php artisan migrate --force
 # Seed the database if needed (Optional: uncomment if you have seeders)
 # php artisan db:seed --force
 
-# Start FrankenPHP and serve the application
+# Start FrankenPHP and serve the application from the public folder
 echo "Starting FrankenPHP..."
-exec frankenphp php-server -l :80
+exec frankenphp php-server -l :80 --root public
