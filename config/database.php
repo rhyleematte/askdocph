7cc6090e38bb9c7d8c899a80ab7a081adb5529ed
rhyleematte <rhyleematte@gmail.com>
2026-04-10 18:48:34 +0800

    fix: add mysql.railway.internal as ultimate database host fallback

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', env('MYSQLHOST', env('MYSQL_HOST', '127.0.0.1'))),
+            'host' => env('DB_HOST', env('MYSQLHOST', env('MYSQL_HOST', 'mysql.railway.internal'))),
             'port' => env('DB_PORT', env('MYSQLPORT', env('MYSQL_PORT', '3306'))),
             'database' => env('DB_DATABASE', env('MYSQLDATABASE', env('MYSQL_DATABASE', 'forge'))),
             'username' => env('DB_USERNAME', env('MYSQLUSER', env('MYSQL_USER', 'forge'))),
